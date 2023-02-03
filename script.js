5bb4896ddd9a2f1dbc49dff030753ac1776f3410
0riioncode <121803202+0riioncode@users.noreply.github.com>
2023-02-03 13:41:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// window.location.href = "https://www.netflix.com/watch/81408738?trackId=14216260"
-window.location.href = "https://www.netflix.com/watch/80003008?trackId=14170287"
+window.location.href = "https://www.netflix.com/watch/81408738?trackId=14216260"
+// window.location.href = "https://www.netflix.com/watch/80003008?trackId=14170287"
