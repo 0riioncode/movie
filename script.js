5b568d372a5560a8a51973d6c01df0d9e95ebe99
0riioncode <121803202+0riioncode@users.noreply.github.com>
2023-09-19 20:07:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location.href = "https://www.netflix.com/watch/70189287?trackId=14036108&tctx=8%2C3%2C9dd98542-6df5-43a9-a260-3d4aa56aaef4-214504513%2Cunknown%2Cunknown%2C%2C%2C%2C%2CVideo%3A70189287%2CdetailsPagePlayButton"
+window.location.href = "https://www.netflix.com/watch/80997419?trackId=14036108&tctx=45%2C3%2C01fc9d15-ce0c-4df0-ac61-3fe12d6c99aa-37776742%2Cunknown%2Cunknown%2C%2C%2C%2C%2CVideo%3A80997419%2CdetailsPagePlayButton"
